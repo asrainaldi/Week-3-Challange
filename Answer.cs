9792209eb688cb2540e04682a3038112777f0a38
asrainaldi <71390370+asrainaldi@users.noreply.github.com>
2020-10-01 20:44:50 +0100

    Update Answer.cs

--- Answer.cs
+++ Answer.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace ConsoleApp2
+namespace ConsoleAppTdpChallangeWeek3
 {
     class Program
     {
@@ -22,51 +22,93 @@ namespace ConsoleApp2
         {
             public static string Tens(int number)
             {
-                string result = "";
                 string units = "";
-                string tens = "";
 
-                tens = Challange.twentyToNinety[((number / 10) - 2)];
+                string tens = Challange.twentyToNinety[((number / 10) - 2)];
 
                 if ((number % 10) > 0)
                     units = Challange.zeroToNineten[(number % 10)];
-                result = tens + " " + units;
+                string result = tens + " " + units;
                 return result;
             }
         }
 
         public class Challange
         {
+            string result = "";
+            string tens = "";
+            string hundreds = "";
             public string NumberConverter(int number)
             {
-                string result = "";
-                string units = "";
-                string tens = "";
 
                 if (number < 20)
+                {
                     result = zeroToNineten[number];
+                }
                 else if (number >= 20 && number <= 99)
                 {
                     result = FindTens.Tens(number);
                 }
                 else if (number >= 100 && number <= 999)
                 {
-                    int numberHunreds = number / 100;
-                    int numberTens = (number - ((number / 100) * 100));
+                    int numberTens = NumberOfTens(number, 100);
+
                     if (numberTens < 20)
                         tens = zeroToNineten[numberTens];
                     else
                     {
                         tens = FindTens.Tens(numberTens);
                     }
-                    result = zeroToNineten[numberHunreds];
+
                     if (numberTens == 0)
-                        result = result + " " + hundred[0];
+                        result = zeroToNineten[(number / 100)] + " " + hundred[0];
                     else
-                        result = result + " " + hundred[0] + " and " + tens;
+                        result = zeroToNineten[(number / 100)] + " " + hundred[0] + " and " + tens;
+                }
+                else if (number >= 1000 && number <= 9999)
+                {
+                    hundreds = "";
+                    int numberoftens = NumberOfTens(number, 100);
+                    int numberofhundred = NumberOfTens(number, 1000) - numberoftens;
+
+                    if (numberoftens < 20)
+                        tens = zeroToNineten[numberoftens];
+                    else
+                    {
+                        tens = FindTens.Tens(numberoftens);
+                    }
+
+                    if (numberofhundred >= 100)
+                        hundreds = zeroToNineten[(numberofhundred / 100)];
+                    //else
+                    //{
+                    //    hundreds = FindTens.Tens((numberofhundred / 10));
+                    //}
+
+                    if (numberoftens < 20)
+                        tens = zeroToNineten[numberoftens];
+                    else
+                    {
+                        tens = FindTens.Tens(numberoftens);
+                    }
+
+
+                    result = zeroToNineten[(number / 1000)] + " " + thousand[0];
+
+                    if(hundreds != "")
+                        result = result + " " + hundreds + " " + hundred[0];
+
+                    if (numberoftens != 0)
+                        result = result +" " + "and " + tens;
+
                 }
 
                 return result;
+            }
+
+            private int NumberOfTens(int number, int size)
+            {
+                return (number - ((number / size) * size));
             }
 
             public static string[] zeroToNineten =
@@ -84,7 +126,7 @@ namespace ConsoleApp2
                 "ten",
                 "eleven",
                 "twelfth",
-                "threeten",
+                "thirteen",
                 "fourteen",
                 "fithteen",
                 "sixteen",
